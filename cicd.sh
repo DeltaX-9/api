806ecfddc78be828fc0147e03cdceaee8bde0d58
Abhisht <abhishtchouhan@gmail.com>
2023-09-24 02:38:16 +0530

    Added CICD

---
+++ cicd.sh
@@ -0,0 +1,8 @@
+#bash ./api/cicd.sh
+# Pulling the latest code from the repository
+git pull https://github.com/DeltaX-9/api.git
+
+
+# Installing the dependencies
+cd ./platform && npm i && cd ../
+
