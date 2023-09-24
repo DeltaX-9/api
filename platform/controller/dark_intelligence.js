147b4e6cd983c2367438750b6cc48c4279eda022
Abhisht <abhishtchouhan@gmail.com>
2023-09-24 10:28:36 +0530

    Platform Updated

--- platform/controller/dark_intelligence.js
+++ platform/controller/dark_intelligence.js
@@ -1,4 +1,5 @@
 const mongoClient = require('../mongo');
+const fs = require('fs');
 
 
 module.exports.basicMetadata = async(req,res) => {
@@ -18,3 +19,51 @@ module.exports.basicMetadata = async(req,res) => {
         return res.status(500).send({message: error.message});
     }
 }
+
+module.exports.getCrawlerData = async(req,res) => {
+  try {
+    const db = mongoClient.db('deltax');
+    const collection = db.collection('info');
+
+    const data = await collection.findOne({
+      key: "crawl-custom"
+    })
+
+    return res.send(data);
+
+  } catch (error) {
+    console.log(error)
+    return res.send({message: error.message});
+  }
+}
+
+module.exports.writeCrawlerInfo = async(req,res) => {
+    try {
+        const filepath = "data.json";
+
+        fs.readFile(filepath, 'utf8', (err, data) => {
+            if (err) {
+              console.error('Error reading JSON file:', err);
+              return;
+            }
+          
+            try {
+              // Parse the JSON data into a JavaScript object
+              const jsonData = JSON.parse(data);
+          
+              // Now, jsonData contains the contents of the JSON file
+              console.log('JSON data from file:', jsonData);
+          
+            } catch (parseError) {
+              console.error('Error parsing JSON:', parseError);
+            }
+          });
+
+        return res.send({message: "done"})
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({message: error.message})
+    }
+}
+
+
--- platform/routes/di.js
+++ platform/routes/di.js
@@ -5,6 +5,10 @@ const di_controller = require('../controller/dark_intelligence');
 
 router.post('/basic-metadata',di_controller.basicMetadata);
 
+router.post('/write-crawler-info',di_controller.writeCrawlerInfo);
+
+router.get('/get-crawler-data',di_controller.getCrawlerData);
+
 module.exports = router;
 
 
